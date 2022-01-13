dde0a90c07d46ecefddcc61bea9a04298ff13b5e
Rob Hess <hessro@oregonstate.edu>
2022-01-13 15:38:13 -0800

    Add code from 1/13 lecture.

--- app/src/main/java/com/example/android/basictodos/MainActivity.kt
+++ app/src/main/java/com/example/android/basictodos/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.android.basictodos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val todoListTV = findViewById<TextView>(R.id.tv_todo_list)
+        val todoEntryET = findViewById<EditText>(R.id.et_todo_entry)
+        val addToDoBtn = findViewById<Button>(R.id.btn_add_todo)
+
+        val toDoList = mutableListOf<String>()
+
+        addToDoBtn.setOnClickListener {
+            val newTodo = todoEntryET.text.toString()
+            if (!TextUtils.isEmpty(newTodo)) {
+                toDoList.add(0, newTodo)
+                todoListTV.text = toDoList.joinToString(separator = "\n\n☐  ", prefix = "☐  ")
+                todoEntryET.setText("")
+            }
+        }
     }
 }
